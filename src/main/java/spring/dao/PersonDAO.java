0fde64570fb6019765b22f67bbc5c718542236f1
GlebEm <gleb.emelyanov.2013@mail.ru>
2023-07-14 14:43:58 +0300

    как же на соплях держится томкэт с мавеном

--- src/main/java/spring/dao/PersonDAO.java
+++ src/main/java/spring/dao/PersonDAO.java
@@ -21,110 +21,18 @@ public class PersonDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-//    private static int PEOPLE_COUNT;
-//    //    private List<Person> people;
-////
-////    {
-////        people = new ArrayList<>();
-////
-////        people.add(new Person(++PEOPLE_COUNT, "Tom",24,"tom@mail.ru"));
-////        people.add(new Person(++PEOPLE_COUNT, "Bob",52,"bob@mail.ru"));
-////        people.add(new Person(++PEOPLE_COUNT, "Mike",18,"mike@mail.ru"));
-////        people.add(new Person(++PEOPLE_COUNT, "Katy",34,"katy@mail.ru"));
-////    }
-//    private static final String URL = "jdbc:postgresql://localhost:5432/firstDB";
-//    private static final String USERNAME = "postgres";
-//    private static final String PASSWORD = "postgres";
-//
-//    private static Connection connection;
-//
-//    static {
-//        try {
-//            Class.forName("org.postgresql.Driver");  //устарело
-//        } catch (ClassNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        try {
-//            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
 
     public List<Person> index() {
-//        List<Person> people = new ArrayList<>();
-//
-//        try {
-//            Statement statement = connection.createStatement();
-//            String SQL = "SELECT*FROM Person";
-//            ResultSet resultSet = statement.executeQuery(SQL);
-//            while (resultSet.next()) {
-//                Person person = new Person();
-//
-//                person.setId(resultSet.getInt("id"));
-//                person.setName(resultSet.getString("name"));
-//                person.setEmail(resultSet.getString("email"));
-//                person.setAge(resultSet.getInt("age"));
-//
-//                people.add(person);
-//            }
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//
-//        }
-//
-//        return people;
         return jdbcTemplate.query("SELECT*FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id) {
-//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-//        Person person = null;
-//
-//        try {
-//            PreparedStatement preparedStatement =
-//                    connection.prepareStatement("SELECT* FROM Person WHERE id=?");
-//            preparedStatement.setInt(1, id);
-//
-//            ResultSet resultSet = preparedStatement.executeQuery();
-//            resultSet.next();
-//
-//            person = new Person();
-//
-//            person.setId(resultSet.getInt("id"));
-//            person.setName(resultSet.getString("name"));
-//            person.setEmail(resultSet.getString("email"));
-//            person.setAge(resultSet.getInt("age"));
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return person;
-
         //  []
         return jdbcTemplate.query("SELECT * FROM Person WHERE ID =?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-//        person.setId(++PEOPLE_COUNT);
-//        people.add(person);
-//
-//        try {
-//            PreparedStatement preparedStatement =
-//                    connection.prepareStatement("INSERT INTO Person VALUES(1,?,?,?)");
-//
-//            preparedStatement.setString(1, person.getName());
-//            preparedStatement.setInt(2, person.getAge());
-//            preparedStatement.setString(3, person.getEmail());
-//
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
         jdbcTemplate.update("INSERT INTO Person VALUES (1,?,?,?)",
                 person.getName(),
                 person.getAge(),
@@ -134,43 +42,12 @@ public class PersonDAO {
     public void update(int id, Person updatePerson) {
         jdbcTemplate.update("UPDATE person SET name=?,age=?,email=? WHERE id =?", updatePerson.getName(),
                 updatePerson.getAge(), updatePerson.getEmail(), id);
-//        try {
-//            PreparedStatement preparedStatement =
-//                    connection.prepareStatement("UPDATE Person SET name =?, age = ?, email=? WHERE id=?");
-//
-//            preparedStatement.setString(1, updatePerson.getName());
-//            preparedStatement.setInt(2, updatePerson.getAge());
-//            preparedStatement.setString(3, updatePerson.getEmail());
-//            preparedStatement.setInt(4,id);
-//
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
 
-//        Person personToBeUpdated = show(id);
-//
-//        personToBeUpdated.setName(updatePerson.getName());
-//        personToBeUpdated.setAge(updatePerson.getAge());
-//        personToBeUpdated.setEmail(updatePerson.getEmail());
     }
 
     public void delete(int id) {
 jdbcTemplate.update("DELETE FROM Person WHERE id=?",id);
 
-//        try {
-//            PreparedStatement preparedStatement =
-//                    connection.prepareStatement("DELETE FROM Person WHERE id=?");
-//
-//            preparedStatement.setInt(1, id);
-//
-//            preparedStatement.executeUpdate();
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-
-//        people.removeIf(p -> p.getId() == id);
     }
 }
 
